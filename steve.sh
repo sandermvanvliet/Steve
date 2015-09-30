c777b8578d47b94e5a1846921c9499840d568bb1
Sander van Vliet <sander@codenizer.nl>
2015-09-30 06:36:09 +0200

    Added queue lookup and request lookup

--- steve.sh
+++ steve.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# vim: set ts=4
 
 VERSION="0.0.1"
 
@@ -11,6 +12,36 @@ echo "Steve version $VERSION"
 
 if [ ! -f $STEVE_CONFIG ]
 then
-  echo "Config not found at $STEVE_CONFIG, refusing to run." > /dev/stderr
+  echo "Config not found at $STEVE_CONFIG, refusing to run" > /dev/stderr
   exit 1
 fi
+
+. $STEVE_CONFIG
+
+if [ -z $QUEUE ]
+then
+  QUEUE=/var/spool/steve
+fi
+
+echo "Looking in queue: $QUEUE"
+
+if [ ! -d $QUEUE ]
+then
+  echo "Queue not found or not a directory" > /dev/stderr
+  exit 1
+fi
+
+NUMBER_OF_REQUESTS=`find $QUEUE -type f | wc -l`
+
+if [ $NUMBER_OF_REQUESTS -eq 0 ]
+then
+  echo "No requests pending"
+  exit 0
+fi
+
+echo "$NUMBER_OF_REQUESTS requests pending"
+
+for request in `ls $QUEUE/*.request | sort -V`
+do
+    echo "Running $request"
+done
