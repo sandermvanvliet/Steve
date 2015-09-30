f277717630db31152bb4b164b3406809ac288dd4
Sander van Vliet <sander@codenizer.nl>
2015-09-30 16:33:08 +0200

    Added test to check if the working directory for a request is created

---
+++ test/WorkingDirectoryIsCreatedForRequest.test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Source test support functions
+. ./test-support.sh
+
+# Source the configuration to get a reference to the queue
+. ./test-steve.conf
+
+#### Arrange goes here
+if [ ! -d $QUEUE ]
+then
+  mkdir $QUEUE
+fi
+
+rm -f $QUEUE/*.request
+
+touch $QUEUE/20150102122020.request
+
+#### Act: Run steve
+./execsteve.sh
+EXITCODE=$?
+
+#### Assert: Check expected results
+AssertEqual $? 0
+
+if [ ! -d $STEVE_WORKING_DIR/20150102122020 ]
+then
+  TestFailure "Working directory not created for request"
+  exit 1
+fi
